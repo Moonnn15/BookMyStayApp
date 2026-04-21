6976753f3498fe6c2d234a23cd920a5e94ba70fd
Moonnn15 <monabhtrig@gmail.com>
2026-04-21 15:21:28 +0530

    Monalisa Added UC2

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,12 +1,19 @@
-public class BookMyStayApp {
+ppublic class BookMyStayApp {
+
     public static void main(String[] args) {
 
         System.out.println("==========================================");
-        System.out.println("        Book My Stay App v1.0");
+        System.out.println("        Book My Stay App v2.0");
         System.out.println("==========================================");
 
         System.out.println("Welcome to the Hotel Booking Management System");
-
         System.out.println("System initialized successfully.");
+        System.out.println();
+
+        // Execute Use Case 2
+        UseCase2RoomInitialization useCase2 =
+                new UseCase2RoomInitialization();
+
+        useCase2.execute();
     }
 }
